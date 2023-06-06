402d592280f2df2f8bb3eddd049321b61bc13611
AY202301PartTimeTeam2 <AY202301PartTimeTeam2@gmail.com>
2023-06-06 17:42:08 -0600

    Adding model in api

--- model_duration/model_core.py
+++ model_duration/model_core.py
@@ -1,3 +1,10 @@
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from xgboost import XGBRegressor
+
+from src import preprocessing as prep
 
 def predict(request):
 
@@ -17,6 +24,24 @@ def predict(request):
         Model predicted the fair of ride and other data
     """
 
-    #TODO Call model to predict based on input
+    duration = 0
 
-    return request['time']
+    filtered_df = prep.preprocessing()
+
+    train_df, test_df = train_test_split(filtered_df, test_size=0.2, random_state=42, shuffle=True)
+
+    X_train = train_df.drop("duration", axis=1)
+    X_test = test_df.drop("duration", axis=1)
+    y_train = train_df["duration"]
+    y_test = test_df["duration"]
+
+    columns = X_train.columns
+    scaler = MinMaxScaler()
+    X_train = pd.DataFrame(scaler.fit_transform(X_train), columns= columns)
+    X_test = pd.DataFrame(scaler.transform(X_test), columns= columns)
+
+    xgreg = XGBRegressor()
+    xgreg.fit(X_train, y_train)
+    predict = xgreg.predict(X_test)
+
+    return predict[0]
