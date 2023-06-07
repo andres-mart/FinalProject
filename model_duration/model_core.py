fd283d7c04bd125c93fc1abc0c085147b0a3549f
AY202301PartTimeTeam2 <AY202301PartTimeTeam2@gmail.com>
2023-06-07 14:20:10 -0600

    adding files to model

--- model_duration/model_core.py
+++ model_duration/model_core.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import os
+import pickle
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
@@ -25,22 +27,30 @@ def predict(request):
 
     duration = 0
 
-    filtered_df = preprocessing()
+    filename = "model_duration.pickle"
+    if os.path.isfile(filename):
+        xgreg = pickle.load(open(filename, 'rb'))
+    else:
+        filtered_df = preprocessing()
 
-    train_df, test_df = train_test_split(filtered_df, test_size=0.2, random_state=42, shuffle=True)
+        train_df, test_df = train_test_split(filtered_df, test_size=0.2, random_state=42, shuffle=True)
 
-    X_train = train_df.drop("duration", axis=1)
-    X_test = test_df.drop("duration", axis=1)
-    y_train = train_df["duration"]
-    y_test = test_df["duration"]
+        X_train = train_df.drop("duration", axis=1)
+        X_test = test_df.drop("duration", axis=1)
+        y_train = train_df["duration"]
+        y_test = test_df["duration"]
 
-    columns = X_train.columns
-    scaler = MinMaxScaler()
-    X_train = pd.DataFrame(scaler.fit_transform(X_train), columns= columns)
-    X_test = pd.DataFrame(scaler.transform(X_test), columns= columns)
+        columns = X_train.columns
+        scaler = MinMaxScaler()
+        X_train = pd.DataFrame(scaler.fit_transform(X_train), columns= columns)
+        X_test = pd.DataFrame(scaler.transform(X_test), columns= columns)
 
-    xgreg = XGBRegressor()
-    xgreg.fit(X_train, y_train)
-    predict = xgreg.predict(X_test)
+        xgreg = XGBRegressor()
+        xgreg.fit(X_train, y_train)
 
-    return predict[0]
+        pickle.dump(xgreg, open(filename, 'wb'))
+
+    predict = xgreg.predict([1])
+    duration = predict[0]
+
+    return duration
