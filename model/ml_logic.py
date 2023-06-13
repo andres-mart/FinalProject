9e8ff13debaca4c6e5b9147e1150c5e6e33dda6d
agustinediz <65187706+agustinediz@users.noreply.github.com>
2023-06-13 17:26:06 -0300

    Add files via upload

--- model/ml_logic.py
+++ model/ml_logic.py
@@ -5,6 +5,12 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from xgboost import XGBRegressor
+import googlemaps
+import requests
+
+
+
+api_key = "AIzaSyCP-1qFF-vI7fGe0H7QiJDCgZuGehYfcBE"
 
 start_point = None
 dest_point = None
@@ -19,22 +25,59 @@ def predict(data,start_point,dest_point,hour_input):
     in a specific hour of the day
     """
 
-    #Estimate trip distance using lat and long
-    lat1 = data[data["PULocationID"].isin([start_point])].PULat.mean()
-    lon1 = data[data["PULocationID"].isin([start_point])].PULong.mean()
-    lat2 = data[data["DOLocationID"].isin([dest_point])].DOLat.mean()
-    lon2 = data[data["DOLocationID"].isin([dest_point])].DOLong.mean()
-            
-    coords_1 = (lat1, lon1)
-    coords_2 = (lat2, lon2)
-    trip_distance = geopy.distance.geodesic(coords_1, coords_2).km
+    # #Estimate trip distance using lat and long
+    gmaps = googlemaps.Client(key=api_key)
+    origin = start_point
+    destination = dest_point
 
-    #Estimate median speed at that given time
-    speed_minutes = data[data["hour_pickup"] == hour_input]["speed_minutes"].median()
+    traffic_result = gmaps.directions(origin=origin, destination=destination, mode="driving", departure_time="now", units="metric")
+    if traffic_result:
+        duration_in_traffic = traffic_result[0]['legs'][0]['duration_in_traffic']['value']
+        duration_in_minutes = duration_in_traffic / 60
+        trip_distance = traffic_result[0]['legs'][0]['distance']['value']
+        trip_distance = trip_distance / 1000
+        speed_minutes = trip_distance/duration_in_minutes
+    else:
+        print("Zone not found, please try with nearest zone.")
 
+    url = "https://apis.tollguru.com/toll/v2/origin-destination-waypoints"
+    
+    new_origin = origin + ", NY 10007 USA"
+    new_dest = destination + ", NY 10007 USA"
+    payload = {
+        "from": {"address": new_origin},
+        "to": {"address": new_dest},
+        "serviceProvider": "here",
+        "vehicle": {
+            "type": "2AxlesAuto",
+            "weight": {
+                "value": 20000,
+                "unit": "pound"
+            },
+            "height": {
+                "value": 7.5,
+                "unit": "meter"
+            },
+            "length": {
+                "value": 7.5,
+                "unit": "meter"
+            },
+            "axles": 4,
+            "emissionClass": "euro_5"
+        }
+    }
+    headers = {
+        "content-type": "application/json",
+        "x-api-key": "bgdb834m3N9jTNhQNhLrjgqRLPgLJJGg"
+    }
+
+    response = requests.request("POST", url, json=payload, headers=headers)
+    data_tolls = response.json()
+
+    toll_cost = data_tolls['routes'][0]['costs']['minimumTollCost']
+    ##########################################################################################
     train_df, test_df = train_test_split(data, test_size=0.2, random_state=42, shuffle=True)
-
-    X_train = train_df[["trip_distance", "speed_minutes"]]
+    X_train = train_df[["trip_distance", "speed_minutes", "duration", "tolls_amount"]]
     y_train = train_df["fare_amount"]
 
     scaler = MinMaxScaler()
@@ -42,7 +85,7 @@ def predict(data,start_point,dest_point,hour_input):
 
     #inputs_fare = {"trip_distance" : trip_distance, "speed_minutes": speed_minutes}
 
-    input_data = pd.DataFrame([[trip_distance, speed_minutes]],columns=["trip_distance", "speed_minutes"])
+    input_data = pd.DataFrame([[trip_distance, speed_minutes, duration_in_minutes, toll_cost]],columns=["trip_distance", "speed_minutes", "duration","tolls_amount"])
 
     #inputs_to_predict_fare = pd.DataFrame([inputs_fare])
     inputs_fare_scaled = scaler.transform(input_data)
@@ -55,7 +98,6 @@ def predict(data,start_point,dest_point,hour_input):
     predict_fare = xgreg.predict(inputs_fare_scaled)
     
     fare = str(predict_fare)
-
     ##########################################################################################
     #Get duration
 
